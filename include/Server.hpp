f30bf868ad7aff846a382f8ad1bde84ab4d65e4a
testxxxxxxxxx <pppppmarcin@gmail.com>
2026-02-19 19:33:12 +0100

    created class Server

---
+++ include/Server.hpp
@@ -0,0 +1,31 @@
+#ifndef SERVER_HPP
+
+#define SERVER_HPP
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+namespace MainServer {
+
+	class Server {
+		int fd, port;
+
+		void init();
+		struct sockadd_in saddr, caddr;
+
+		public:
+			
+			Server(int port): port(port) {
+				this->saddr = {};
+				this->caddr = {};	
+			}
+			~Server() {
+
+			}
+
+			int acceptConnection();
+			void setnonblock(int fd);
+	};
+};
+#endif
---
+++ src/Server.cpp
@@ -0,0 +1,12 @@
+#include "../include/Server.hpp"
+#include <fcntl.h>
+
+void MainServer::Server::init() {
+
+}
+int MainServer::Server::acceptConnection() {
+
+}
+void MainServer::Server::setnonblock(int fd) {
+
+}
